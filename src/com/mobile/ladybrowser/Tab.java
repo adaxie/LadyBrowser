e6faedd6536819fa881659a6cf049ea077c35e58
adaxie <ada11211@163.com>
2015-03-17 17:57:06 +0800

    3-17, webview&titlebar working on

--- src/com/mobile/ladybrowser/Tab.java
+++ src/com/mobile/ladybrowser/Tab.java
@@ -1,24 +1,45 @@
 package com.mobile.ladybrowser;
 
-import android.content.Context;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Tab {
-    
+
     private String TAG = "LadyBrowser-Tab";
 
     final private Controller mController;
-    final private WebView mWebView;
+    private WebView mWebView;
     
     public Tab(WebView w, Controller controller) {
-        mWebView = w;
+        setWebView(w);
         mController = controller;
     }
-    
+
+    final private WebViewClient mWebViewClient = new WebViewClient(){
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+    };
+
+    final private WebChromeClient mWebChromeClient = new WebChromeClient() {
+
+    };
+
+    private void setWebView(WebView w) {
+        mWebView = w;
+        w.setWebChromeClient(mWebChromeClient);
+        w.setWebViewClient(mWebViewClient);
+    }
+
     public WebView getWebView() {
         return mWebView;
     }
-    
+
     public void loadUrl(String url) {
         mWebView.loadUrl(url);
     }
